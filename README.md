24c54faa806550ce6ec0dac9ef7bc6c88f88710b
ADVAIT GURUNATH CHAVAN <59262727+ADVAIT135@users.noreply.github.com>
2021-07-27 16:15:21 +0530

    School Result Management System(SRMS)
    This commit consists of all html and php files needed for SRMS

---
+++ README.md
@@ -0,0 +1,3 @@
+Made By Advait
+Phone : 7021455852
+email: advaitchavan135@gmail.com
---
+++ dashboard.php
@@ -0,0 +1,65 @@
+
+        
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/home.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="./css/font-awesome-4.7.0/css/font-awesome.css">
+    <link rel="stylesheet" href="normalize.css">
+    <title>Dashboard</title>
+    <style>
+        .main{
+            border-radius: 10px;
+            border-width: 5px;
+            border-style: solid;
+            padding: 20px;
+            margin: 7% 20% 0 20%;
+        }
+    </style>
+</head>
+<body>
+        
+    <div class="title">
+        <a href="dashboard.html"><img src="./images/logo1.png" alt="" class="logo"></a>
+        <span class="heading">Dashboard</span>
+        <a href="logout.html" style="color: white"><span class="fa fa-sign-out fa-2x">Logout</span></a>
+    </div>
+
+    <div class="nav">
+        <ul>
+            <li class="dropdown" onclick="toggleDisplay('1')">
+                <a href="" class="dropbtn">Classes &nbsp
+                    <span class="fa fa-angle-down"></span>
+                </a>
+                <div class="dropdown-content" id="1">
+                    <a href="add_classes.html">Add Class</a>
+                    <a href="manage_classes.html">Manage Class</a>
+                </div>
+            </li>
+            <li class="dropdown" onclick="toggleDisplay('2')">
+                <a href="#" class="dropbtn">Students &nbsp
+                    <span class="fa fa-angle-down"></span>
+                </a>
+                <div class="dropdown-content" id="2">
+                    <a href="add_students.html">Add Students</a>
+                    <a href="manage_students.html">Manage Students</a>
+                </div>
+            </li>
+            <li class="dropdown" onclick="toggleDisplay('3')">
+                <a href="#" class="dropbtn">Results &nbsp
+                    <span class="fa fa-angle-down"></span>
+                </a>
+                <div class="dropdown-content" id="3">
+                    <a href="add_results.html">Add Results</a>
+                    <a href="manage_results.html">Manage Results</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+
+    
+</body>
+</html>
